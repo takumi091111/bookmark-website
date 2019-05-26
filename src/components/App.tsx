6af0397325777632b94a0026f64d26385dea5035
Asamac <takumi091111@gmail.com>
2019-05-27 07:46:23 +0900

    Google Fontsからフォントを読み込むように変更

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,5 @@
-import * as React from 'react'
+/** @jsx jsx */
+import { jsx, css, Global } from '@emotion/core'
 import { Provider } from 'unstated'
 
 import { Layout } from './Layout'
@@ -10,8 +11,15 @@ import { SlidePanelContainer } from './SlidePanel'
 import { TagListContainer } from './TagList'
 import { BookmarkListContainer } from './BookmarkList'
 
+const globalStyle = css`
+  html, body {
+    font-family: 'Noto Serif', serif;
+  }
+`
+
 const App = (): JSX.Element => (
   <Provider>
+    <Global styles={ globalStyle } />
     <Layout>
       <HeaderContainer />
       <SideBarContainer>
