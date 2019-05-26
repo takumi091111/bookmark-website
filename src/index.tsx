77d4fb9d981b54529a07e1ff62d5873f54378f88
Asamac <takumi091111@gmail.com>
2019-05-27 02:41:32 +0900

    unstated-debugを読み込むように変更

--- src/index.tsx
+++ src/index.tsx
@@ -3,5 +3,6 @@ import { render } from 'react-dom'
 import App from './components/App'
 
 import 'minireset.css'
+const UNSTATED = require('unstated-debug') // eslint-disable-line no-unused-vars
 
 render(<App />, document.getElementById('app'))
